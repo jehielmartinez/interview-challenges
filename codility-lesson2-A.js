dea1f8eb74df24a9262ccb066b24f749280faf91
Jehiel Martinez <jehielmartinez@gmail.com>
2019-09-18 18:29:23 -0600

    changed name

---
+++ codility-lesson2-A.js
@@ -0,0 +1,39 @@
+function OddOcurrences(A){
+    let len = A.length;
+    A.sort()
+
+    for(i=0; i<len; i++){
+        if(A[i] != A[i+1] && A[i] != A[i-1]){
+            return A[i]
+        }
+    }
+
+    return -1
+}
+
+//Testing
+
+const cases = [
+    {
+        label: 'Given',
+        input: OddOcurrences([9,3,9,3,9,7,9]),
+        shouldBe: 7
+    },
+    {
+        label: 'Last position answer',
+        input: OddOcurrences([5,3,5,3,5,9,5]),
+        shouldBe: 9
+    },
+    {
+        label: 'First position answer',
+        input: OddOcurrences([5,3,5,9,5,9,5]),
+        shouldBe: 3
+    },
+    {
+        label: 'No answer',
+        input: OddOcurrences([5,3,3,9,5,9,5]),
+        shouldBe: -1
+    },
+]
+
+cases.map(test => console.log((test.input === test.shouldBe ? '✅' : '❗') + "  " + test.label + " => " + "Should be: " + test.shouldBe + " received: " + test.input))
