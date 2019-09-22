611cd601a3e3578ad8766a55f38c0029a84653aa
Jehiel Martinez <jehielmartinez@gmail.com>
2019-09-22 13:01:00 -0600

    squareRoot challeange using Babylonian Aprox completed

--- @cassidoo_20-09.js
+++ @cassidoo_20-09.js
@@ -1,15 +1,33 @@
 //Implement a square root function (without using an exponential function).
 
-function squareRoot(num) {
- return Math.sqrt(num)
+function squareRoot(S, K) {
+    //using Babylonian aproximation
+    let Xm = K * 10 * 10;
+
+    for(i=0; i<K; i++){
+        Xm = (Xm + S/Xm)/2
+        console.log(Xm)
+    }
+
+    return Xm.toFixed(3)
 }
 
 //Testing
 const cases = [
     {
-        label: 'Number 25',
-        input: squareRoot(25),
+        label: 'S=25, K=10 ',
+        input: squareRoot(25, 10),
         shouldBe: 5
+    },
+    {
+        label: 'S=125348, K=10',
+        input: squareRoot(125348, 10),
+        shouldBe: 354.045
+    },
+    {
+        label: 'S=64589, K=10',
+        input: squareRoot(64589, 10),
+        shouldBe: 254.144
     }
 ]
 
