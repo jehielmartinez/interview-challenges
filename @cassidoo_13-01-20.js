42bb644345c0a2434f26e9a697f6a990091f2ef5
Jehiel Martinez <jehielmartinez@Jehiels-MacBook-Pro.local>
2020-01-13 15:32:48 -0600

    cassidoo interview challenge 13-01-20

---
+++ @cassidoo_13-01-20.js
@@ -0,0 +1,28 @@
+//Given a number n, find the sum of all n-digit palindromes.
+//495 => 11 + 22 + 33 + 44 + 55 + 66 + 77 + 88 + 99
+
+function palindromes(n) {
+    let result = 0
+    for(i=1; i<10; i++){
+        let srtMult = '1'.repeat(n);
+
+        result = result + i * parseInt(srtMult)
+    }
+    return result
+}
+
+//Testing
+const cases = [
+    {
+        label: '2',
+        input: palindromes(2),
+        shouldBe: 495
+    },
+    {
+        label: '3',
+        input: palindromes(3),
+        shouldBe: 4995
+    },
+]
+
+cases.map(test => console.log((test.input == test.shouldBe ? '✅' : '❗') + "  " + test.label + " => " + "Should be: " + test.shouldBe + " received: " + test.input))
